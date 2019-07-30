34124b9b95fdcc948ab2f09415fce13626ee1abc
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-07-30 12:17:57 +0100

    reusable search-box component and filter monsters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { CardList } from '../src/components/card-list/card-list.component';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -18,9 +20,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
+
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox
+          placeholder='search monster'
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
