83e1f04ffac4dc242971bbd43ed8bf1f1a0631d9
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-04 18:36:46 +0100

    Updates

---
+++ precache-manifest.3b57a2b130be99dfa55e28eafb470e10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4da0d81627df142ed008de037019443",
+    "url": "/monster-roller/index.html"
+  },
+  {
+    "revision": "7afcda940eded2065cf7",
+    "url": "/monster-roller/static/css/main.f2acbd44.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-roller/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7afcda940eded2065cf7",
+    "url": "/monster-roller/static/js/main.69d824ec.chunk.js"
+  },
+  {
+    "revision": "673248b5f248419d54ec",
+    "url": "/monster-roller/static/js/runtime~main.e6c5f182.js"
+  }
+]);
